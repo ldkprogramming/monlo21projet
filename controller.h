31b61a4eac95ad71fc76c8b97d1f8df59617ebeb
Leonardo <ldkprogramming@gmail.com>
2023-11-22 11:26:33 +0100

    Ajout de l'ebauche de la classe Controller

--- controller.h
+++ controller.h
@@ -5,10 +5,34 @@
 #ifndef MONLO21PROJET_CONTROLLER_H
 #define MONLO21PROJET_CONTROLLER_H
 #include "game.h"
+#include "player.h"
 
 class Controller {
 public:
-    
+    // Retourne les actions optionnelles possibles
+    std::vector<OptionalActions> getValidOptionalActions(const Game& game);
+
+    // Retourne les actions obbligatoires possibles
+    std::vector<CompulsoryActions> getValidCompulsoryActions (const Game& game);
+
+    // appliquer une action optionnelle
+    void applyOptionalAction(Game& game, OptionalActions action);
+
+    // appliquer une action obbligatoire
+    void applyCompulsoryAction(Game& game, CompulsoryActions action);
+
+    // appliquer la capacite d'une carte
+    void applyCardSkill(Game& game, Player& cardOwner, Card& card);
+
+    // Jouer un jeu splendor duel
+    void play(Game& game);
+
+    // Jouer le tour d'un joueur
+    void playTurn(Game& game, Player& player);
+
+    // Verifier si un Joueur gagne
+    bool checkIfPlayerWins(Game& game, Player& player);
+
 };
 
 
