c92829900639566238131c594edc364209429c2f
bannedtwice <150385688+bannedtwice@users.noreply.github.com>
2023-11-30 20:58:34 +0000

    #include

--- GameMoveVerification.h
+++ GameMoveVerification.h
@@ -1,6 +1,18 @@
 #pragma once
 
 #include "pile.h"
-#include ""
+#include "card.h"
+#include "game.h"
+#include "cardpyramid.h"
+#include "coin.h"
+#include "coinbag.h"
+#include "coinboard.h"
+#include "controller.h"
+#include "player.h"
+#include "winconditions.h"
 
-class GameMoveVerification()
+class GameMoveVerification{
+
+
+
+};
