7c6baa2beb9a75ff1d3aa26483e423c18b8115a8
bannedtwice <150385688+bannedtwice@users.noreply.github.com>
2023-12-15 23:06:12 +0100

    Update controller.h

--- controller.h
+++ controller.h
@@ -7,35 +7,32 @@
 #include "game.h"
 #include "player.h"
 #include "gamesaver.h"
+#include "GameMoveVerification.h"
 
 class Controller {
+private: 
+	friend class Game;
+	friend class GameMoveVerification;
+	GameMoveVerification& checker;
+	const Game& GameControlled;
+
+
 public:
-    // faut completement refaire
-    /*
-    // Retourne les actions optionnelles possibles pour un joueur
-    std::vector<OptionalActions> getValidOptionalActions(const Game& game, Player& player) const;
+	Controller(GameMoveVerification& checker, const Game& GameControlled);
+	Controller(const Controller&) = delete;
+	Controller& operator=(const Controller&) = delete;
 
-    // Retourne les actions obbligatoires possibles pour un joueur
-    std::vector<CompulsoryActions> getValidCompulsoryActions (const Game& game, Player& player) const;
+	inline GameMoveVerification& get_checker() const { return checker; }
+	inline const Game&  get_GameControlled() const { return GameControlled; }
 
-    // appliquer une action optionnelle
-    void applyOptionalAction(Game& game, Player& player,  OptionalActions action);
+	bool launch_save(GameSaver& save);
+	bool reset_game();
 
-    // appliquer une action obbligatoire
-    void applyCompulsoryAction(Game& game, Player& player,  CompulsoryActions action);
+	void play_game();
+	void change_turn();
+	bool verify_win(Player& player);
 
-    // appliquer les capacites d'une carte
-    bool applyCardSkills(Game& game, Player& cardOwner, Player& opponent, Card& card);
 
-    // Jouer un jeu splendor duel
-    void play(Game& game);
-
-    // Jouer le tour d'un joueur
-    void playTurn(Game& game, Player& player);
-
-    // Verifier si un Joueur gagne
-    bool checkIfPlayerWins(Game& game, Player& player);
-     */
 };
 
 
