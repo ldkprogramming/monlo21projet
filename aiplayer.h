c17e5720a3b4e25126db3ab53594db7a23601af9
bannedtwice <gitproject12@proton.me>
2023-12-15 23:30:43 +0100

    Classe AI héritage

--- aiplayer.h
+++ aiplayer.h
@@ -1,1 +1,18 @@
+#pragma once
+#include "game.h"
+#include "GameMoveVerification.h"
+#include "player.h"
+#include "coin.h"
+#include "coinboard.h"
+#include "coinbag.h"
+#include "card.h"
+#include "cardpyramid.h"
+#include "pile.h"
+#include "player.h"
+#include <cstdlib>
+#include <cstdio>
+
+class AIPlayer : public Player{
+	
+};
 
