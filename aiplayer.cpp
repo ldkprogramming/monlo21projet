1e3b59fefe61422b430199e1b9e4c035d59fd12a
bannedtwice <gitproject12@proton.me>
2023-12-17 00:37:41 +0100

    Début AIplayer

--- aiplayer.cpp
+++ aiplayer.cpp
@@ -1,5 +1,9 @@
 #include "aiplayer.h"
 
-AIPlayer::AIPlayer(std::name)
+std::vector<std::pair<int, int>> AIPlayer::AI_take_coins_by_coordinates(Controller& controller)
 {
+	std::vector<std::pair<int, int>> coordinates = { 0,0 ; }
+	while (controller.get_checker().verify_coin_alignment())
+
+	return std::vector<std::pair<int, int>>();
 }
