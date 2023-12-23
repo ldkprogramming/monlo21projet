52573ab12d55f5d4d1440545e954c4936fcaeca1
bannedtwice <gitproject12@proton.me>
2023-12-23 21:25:18 +0100

    crowns

--- card.cpp
+++ card.cpp
@@ -83,6 +83,7 @@ std::ostream& operator<<(std::ostream& f, const Card& c){
     f << "skill1 : " << toString(c.skill1) << '\n';
     f << "skill2 : " << toString(c.skill2) << '\n';
     f << "Points : " << c.getPoints() << std::endl;
+    f << "Crowns : " << c.getCrowns() << std::endl;
     f << "Couleur : " << toString(c.getCardColor()) << std::endl;
     f << "Cout : " << std::endl;
  
